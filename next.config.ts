fbd95b329046ab8c372776a7f20151381b75c900
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-11-16 23:54:43 +0100

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,29 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: '*',
+        hostname: "*",
       },
     ],
   },
   experimental: {
     ppr: "incremental",
     after: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
   },
 }
 
